f56800f0a86c9dbb1ccde133d2aedb574f748fc6
ather1 <mohammed_ather@hotmail.com>
2018-02-03 22:02:31 +0000

    Atherm

--- routes/tenants.js
+++ routes/tenants.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://atherm:h3110@ds223578.mlab.com:23578/tims',['tenants']);
+var db = mongojs('mongodb://Atherm:h3110@ds223578.mlab.com:23578/tims',['tenants']);
 
 //Get All Tasks
 router.get('/tenants',function(req,res,next){
