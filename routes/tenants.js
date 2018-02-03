18ef7bddfbb6cca1fb127a0f8aeb86fed66ade8d
ather1 <mohammed_ather@hotmail.com>
2018-02-03 22:01:50 +0000

    First

---
+++ routes/tenants.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://atherm:h3110@ds223578.mlab.com:23578/tims',['tenants']);
+
+//Get All Tasks
+router.get('/tenants',function(req,res,next){
+    db.tenants.find(function(err,tenants){
+        if(err){
+            res.send(err);
+
+        }
+        res.json(tenants);
+    });
+});
+
+
+//Get Single tasks
+// router.get('/tenants/:id',function(req,res,next){
+//     db.tenants.findOne(_id: mongojs.Ofunction(err,tenants){
+//         if(err){
+//             res.send(err);
+
+//         }
+//         res.json(tenants);
+//     });
+// });
+
+module.exports = router;
+
